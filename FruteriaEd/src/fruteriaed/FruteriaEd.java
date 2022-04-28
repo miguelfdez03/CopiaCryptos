cd42e8978b240821c391c7f8e51194ca48d163f1
miguel <mfm0015@alu.medac.es>
2022-04-28 12:27:44 +0200

    menu y modificacion de error

--- FruteriaEd/src/fruteriaed/FruteriaEd.java
+++ FruteriaEd/src/fruteriaed/FruteriaEd.java
@@ -21,7 +21,22 @@ public class FruteriaEd {
             System.out.println("6. Salir.");
             System.out.print("opción? >>");
             opcion = scanner.nextInt();
-
+            
+            switch(opcion){
+                case 1:
+                    s.despachar();
+                    break;
+                case 2: 
+                    s.adelantar();
+                    break;
+                case 3:
+                    s.retrasar();
+                case 4:
+                    s.mostrar();
+                case 5:
+                    s.mostrarDespachados();
+            }
+                    
         } while (opcion != 6);
         //salgo del bucle cuando ponga 6
         System.out.println("");
